9c0d3f9eb35895d9cfa8761b9c63edafff961ac0
Brian <briandsalemi@gmail.com>
2019-08-06 15:09:31 -0400

    added helpers to ApplicationController for methods that check if the user is the current user or if they are logged in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,6 +23,14 @@ class UsersController < ApplicationController
         @user = User.find(session[:user_id])
     end
 
+    def edit 
+
+    end 
+
+    def update 
+
+    end 
+
 
     private 
 
