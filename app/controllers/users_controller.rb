c62b75e29563c7ff4232d291658d8a57f5c0ed74
Brian <briandsalemi@gmail.com>
2019-08-06 12:56:58 -0400

    added strong params for User model, created post action for User Sign Up, added view page for User#Show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,24 @@ class UsersController < ApplicationController
     end 
 
     def create 
+        @user = User.new(user_params)
+        if @user.valid?
+           @user.save
+           session[:user_id] = @user.id 
+           redirect_to user_path(@user)
+        else 
+            render :new 
+        end
+    end 
 
+    def show 
+        @user = User.find(session[:user_id])
+    end
+
+
+    private 
+
+    def user_params 
+        params.require(:user).permit(:username, :email, :password, :password_confirmation)
     end 
 end
