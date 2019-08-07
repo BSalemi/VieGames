9480d7d471a33b2fcb2a449cc8ede5e4c7354031
Brian <briandsalemi@gmail.com>
2019-08-07 09:02:00 -0400

    added validations for Game and Event model, add index page for Games

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,12 @@
 class Event < ApplicationRecord
+    validates: (:dates, presence: => true)
+    validates: (:max_num_entrants, presence: => true)
+    validates: (:entrance_fee, presence: => true)
+    validates: (:prize, presence: => true)
+    validates: (:host, presence: => true)
+    validates: (:game_id, presence: => true)
+    validates: (:location, inclusion: { in: [In Person, Online]})
+
     belongs_to :game 
     has_many :user_events 
     has_many :users, through: :user_events
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,6 +1,17 @@
 class Game < ApplicationRecord
+    validates(:title, :presence => true)
+    validates :title, uniqueness: {
+        scope: %i[system_id],
+        message: 'has already been added for this system'
+    }
+
+    validates(:content_rating, :presence => true)
+    validates(:system_id, :presence => true)
+    validates(:company_id, :presence => true)
+
     belongs_to :company 
     belongs_to :system
+
     has_many :events 
     has_many :user_games
     has_many :users, through: :user_games 
