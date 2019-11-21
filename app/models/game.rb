6e2723730a19c84144c698644b30b8b1ca3e674e
Brian <briandsalemi@gmail.com>
2019-11-21 12:34:51 -0500

    added dependent destroy to events to delete events when games are deleted and added image to system show page

--- app/models/game.rb
+++ app/models/game.rb
@@ -12,8 +12,8 @@ class Game < ApplicationRecord
     belongs_to :company 
     belongs_to :system
 
-    has_many :events 
-    has_many :user_games
+    has_many :events, dependent: :destroy
+    has_many :user_games, dependent: :destroy
     has_many :users, through: :user_games 
 
     def average_rating 
